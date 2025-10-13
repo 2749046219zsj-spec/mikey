ba472f0b3c99a78bf2d2d8030a846eabab6c1db8
2749046219zsj-spec <2749046219zsj@gmail.com>
2025-10-13 13:43:14 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ChatContainer } from './components/ChatContainer';
 import { ChatInput } from './components/ChatInput';
 import { ChatHeader } from './components/ChatHeader';
